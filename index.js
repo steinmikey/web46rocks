c78a70dc6492554b68f1bc1ea466e423022b845f
Michael Steiner <michaelvsteiner@gmail.com>
2021-09-30 14:54:01 -0600

    adds cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const path = require("path");
+const cors = require("cors");
 
 console.log(process.argv[2]);
 console.log(process.env.USER);
@@ -7,15 +8,16 @@ console.log(process.env.USER);
 const express = require("express");
 
 const server = express();
+server.use(cors());
 server.use(express.json());
-server.use(express.static(path.join(_dirname, "client/build"))); //needs absolute path to build folder inside client
+server.use(express.static(path.join(__dirname, "client/build"))); //needs absolute path to build folder inside client
 
-server.get("*", (req, res) => {
-  res.sendFile(path.join(_dirname, "client/build", "index.html"));
+server.get("/api/users", (req, res) => {
+  res.json([{ id: 1, name: Leo }]);
 });
 
-server.get("/hello", (req, res) => {
-  res.send("<h1>HELLO THERE</h1>");
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 const port = process.env.PORT || 3000;
