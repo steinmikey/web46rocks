650f536ec3e482bceb5d15b748c334fe184161d3
Michael Steiner <michaelvsteiner@gmail.com>
2021-09-30 11:46:39 -0600

    bring in react

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
+const path = require("path");
 
-console.log("web46 rulez");
 console.log(process.argv[2]);
-console.log(process.argv[3]);
 console.log(process.env.USER);
 
 const express = require("express");
 
 const server = express();
 server.use(express.json());
+server.use(express.static(path.join(_dirname, "client/build"))); //needs absolute path to build folder inside client
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "client/build", "index.html"));
+});
 
 server.get("/hello", (req, res) => {
   res.send("<h1>HELLO THERE</h1>");
